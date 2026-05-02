de257ca460aae87c3ab4c6bb90c040cf11d57f35
HalfTrapped <asianwater1@gmail.com>
2026-05-02 09:58:03 +0800

    Done adding react native stack

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import TodoScreen from "./screens/TodoScreen";
 import LoginScreen from "./screens/LoginScreen";
+import SignupScreen from "./screens/SignupScreen";
 import { useState } from "react";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Id } from "./convex/_generated/dataModel";
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
